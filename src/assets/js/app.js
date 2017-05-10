08956b2d3771aa81c84820480f07cbdcf9a95850
Szabó Gábor <szabogabor@hydrogene.hu>
2017-05-10 17:21:02 +0200

    owl added

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,28 @@
 $(document).foundation();
+
+$(document).ready(function(){
+
+  $('.wseries__carousel').owlCarousel({
+    margin:20,
+    dots:false,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:1,
+            margin:30,
+        },
+        768:{
+            items:2,
+        },
+        1024:{
+            items:3,
+            margin: 40,
+            nav:true,
+            dots:true
+        }
+    }
+  });
+});
